f2e53724ec79358a35ffb9ea63f62d90aa342429
jsillero-ga <jsillerosalado@gmail.com>
2024-10-30 12:07:54 +0100

    Current state

--- src/services/backendConnection.js
+++ src/services/backendConnection.js
@@ -1,7 +1,10 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`;
+import axios from '../services/interceptors'
+import { getToken } from "../utils/auth";
 
 const index = async () => {
     try {
+        const { data } = await axios.post(`${BASE_URL}/signup`, formData)
         const res = await fetch(BASE_URL);
         return res.json();
     } catch (err) {
@@ -9,6 +12,16 @@ const index = async () => {
     }
 };
 
+//transactions requests
+const getTransactions = async ( ) => {
+    try {
+         
+        const { data } = await axios.get(`${BASE_URL}/transactions`);
 
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-export { index };
+export { index, getTransactions };
