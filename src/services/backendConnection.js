c8591b4d9298195db43486c47cf5f8b31ba76cde
jsillero-ga <jsillerosalado@gmail.com>
2024-10-30 15:52:32 +0100

    Added new backend connections

--- src/services/backendConnection.js
+++ src/services/backendConnection.js
@@ -2,16 +2,6 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`;
 import axios from '../services/interceptors'
 import { getToken } from "../utils/auth";
 
-const index = async () => {
-    try {
-        const { data } = await axios.post(`${BASE_URL}/signup`, formData)
-        const res = await fetch(BASE_URL);
-        return res.json();
-    } catch (err) {
-        console.log(err);
-    }
-};
-
 //transactions requests
 const getTransactions = async () => {
     try {
@@ -28,6 +18,17 @@ const createTransaction = async (formData) => {
     try {
 
         const { data } = await axios.post(`${BASE_URL}/transactions`, formData);
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const singleTransaction = async (id) => {
+    try {
+
+        const { data } = await axios.get(`${BASE_URL}/transactions/${id}`);
 
         return data;
     } catch (error) {
